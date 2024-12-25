f36c67c8cb7942c960ab1b1c6156d414cec14652
lakshya1goel <lakshya1234goel@gmail.com>
2024-12-25 13:23:20 +0530

    bottom tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,28 @@
-import { Stack } from "expo-router";
+// Using modal and Stack
+// import { Stack } from "expo-router";
+
+// export default function Layout (){
+//     return (
+//         <Stack>
+//             <Stack.Screen name="index" options={{title:"Shopping List"}} />
+//             <Stack.Screen name="counter" options={{title:"Counter", presentation:"modal", animation:"slide_from_bottom"}} />
+//             <Stack.Screen name="idea" options={{title:"Idea"}} />
+//         </Stack>
+//     )
+// }
+
+import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { theme } from "../theme";
 
 export default function Layout (){
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{title:"Shopping List"}} />
-        </Stack>
+        <Tabs screenOptions={{tabBarActiveTintColor:theme.colorCerulean}}>
+            <Tabs.Screen name="index" options={{title:"Shopping List", tabBarIcon: ({color, size}) =>{return <Feather name="list" size={size} color={color} />}}} />
+            <Tabs.Screen name="counter" options={{title:"Counter", tabBarIcon: ({color, size}) => {return <AntDesign name="clockcircleo" size={size} color={color} />}}} />
+            <Tabs.Screen name="idea" options={{title:"Idea", tabBarIcon: ({color, size}) => {return <FontAwesome5 name="lightbulb" size={size} color={color} />}}} />
+        </Tabs>
     )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* <View style={[StyleSheet.absoluteFill, {backgroundColor: "yellow"}]}/> */}
-      <View style={{ backgroundColor: "yellow", ...StyleSheet.absoluteFillObject }} />
+      {/* <View style={{ backgroundColor: "yellow", ...StyleSheet.absoluteFillObject }} /> */}
       <Link href="/counter" style={{textAlign: "center", marginBottom: 18, fontSize: 24}}> Go to counter </Link>
       <ShoppingListItem name="hi" />
       <ShoppingListItem name="bye" isCompleted={true} />
